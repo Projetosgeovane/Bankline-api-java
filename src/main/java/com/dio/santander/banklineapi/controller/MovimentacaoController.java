7c24db65b5b036f40ce91fd38ddeac6a1aab06b6
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-05-31 21:51:05 -0300

    Api Bankline desenvolvida com a linguagem Java

---
+++ src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
@@ -0,0 +1,41 @@
+package com.dio.santander.banklineapi.controller;
+
+import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.dto.NovoCorrentista;
+import com.dio.santander.banklineapi.model.Correntista;
+
+import com.dio.santander.banklineapi.model.Movimentacao;
+import com.dio.santander.banklineapi.repository.MovimentacaoRepository;
+import com.dio.santander.banklineapi.services.CorrentistaService;
+import com.dio.santander.banklineapi.services.MovimentacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movimentacoes")
+public class MovimentacaoController {
+
+    @Autowired
+    private MovimentacaoRepository repository;
+
+    @Autowired
+    private MovimentacaoService service;
+
+    @GetMapping
+    public List<Movimentacao> findAll(){
+    return repository.findAll();
+    }
+
+    @GetMapping("/{idConta}")
+    public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+        return repository.findByIdConta(idConta);
+    }
+
+    @PostMapping
+    public void save(@RequestBody NovaMovimentacao movimentacao) {
+
+        service.save(movimentacao);
+    }
+}
